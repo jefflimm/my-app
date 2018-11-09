da5608a03f9b1f3959443e89e74ca3329e5259f7
Jeffrey Lim <jeffrey.c.lim@gmail.com>
2018-11-09 10:34:59 -0800

    Enter

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,10 @@ const IndexPage = () => (
   <div>
     <div className="Hero">
       <div className="HeroGroup"></div>
+      <h1>Learn to Design and Code React Apps</h1>
+      <p>This is a test for when I create prototypes</p>
+      <Link to="/page-2/">Go to page 2</Link>
     </div>
-    <h1>Learn to Design and Code React Apps</h1>
-    <p>This is a test for when I create prototypes</p>
-    <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
 
