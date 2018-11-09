9ec6f733c1134b75bf3a99ecf09af442589f672b
Jeffrey Lim <jeffrey.c.lim@gmail.com>
2018-11-09 10:29:36 -0800

    Enter

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,11 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
+    <div className="Hero">
+      <div className="HeroGroup"></div>
+    </div>
     <h1>Learn to Design and Code React Apps</h1>
-    <p>This is a test for when I create my new portfolio</p>
+    <p>This is a test for when I create prototypes</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
